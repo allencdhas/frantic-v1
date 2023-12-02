152cbd33a6b025d9da52c2e89beb664e2f5e22d7
krishnan74 <divyakrishnan.r74@gmail.com>
2023-12-02 19:08:23 +0530

    hero section v1

---
+++ components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between bg-white text-black h-[120px] items-center px-[60px]">
+      <div className="flex">
+        <img src="/franticLogoV2.png" alt="" height={"50"} width={"50"} />
+        <p className=" tracking-widest font-bold text-1xl ml-2 italic ">
+          FRANTIC
+        </p>
+      </div>
+
+      <div className="flex justify-center gap-8 items-center text-sm">
+        <p className="text-purple-500 font-bold">Home</p>
+        {/* <img
+          src="/ethpurp.png"
+          alt=""
+          className="h-[30px] w-[30px] ethpic"
+        /> */}
+        <p>Services</p>
+        <p>About</p>
+      </div>
+
+      <div className="flex justify-end gap-8 items-center">
+        <button className="bg-purple-600 px-5 py-2 text-white font-bold rounded-md text-sm">
+          Contact
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar
---
+++ components/ServiceCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ServiceCard = (props) => {
+  return (
+    <div className='w-[400px] h-[200px] border border-gray-300 px-5 py-5' style={{boxShadow: "2px 2px 0px 0px gray"}}>
+        <p className='text-xl font-semibold tracking-wider'>Networking</p>
+    </div>
+  )
+}
+
+export default ServiceCard
